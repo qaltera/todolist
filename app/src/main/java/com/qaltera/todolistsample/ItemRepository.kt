5c317abbce64b6bca541ec39ff9f2fae3d7ca08c
Yulia Rogovaya <yrogovaya@spbtv.com>
2020-09-24 02:56:52 +0300

    some cleanup

--- app/src/main/java/com/qaltera/todolistsample/ItemRepository.kt
+++ app/src/main/java/com/qaltera/todolistsample/ItemRepository.kt
@@ -12,35 +12,25 @@ import kotlinx.coroutines.CoroutineScope
  * ************************************************
  */
 
-class ItemRepository(
-    context: Context,
-    scope: CoroutineScope
-) {
+class ItemRepository(context: Context, scope: CoroutineScope) {
     private val itemDao: ItemDao
     val allItems: LiveData<List<Item>>
 
     init {
-        val database: ItemDatabase = ItemDatabase.getDatabase(
-            context,
-            scope
-        )
+        val database: ItemDatabase = ItemDatabase.getDatabase(context, scope)
         itemDao = database.itemDao()
         allItems = itemDao.allItems()
     }
 
-    suspend fun insert(item: Item) {
+    fun insert(item: Item) {
         itemDao.insert(item)
     }
 
-    suspend fun update(item: Item) {
+    fun update(item: Item) {
         itemDao.update(item)
     }
 
-    suspend fun delete(item: Item) {
+    fun delete(item: Item) {
         itemDao.delete(item)
-    }
-
-    suspend fun deleteAllItems() {
-        itemDao.deleteAllItems()
     }
 }
